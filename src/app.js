662569a41f35fd26b8192cea477850c84c836b8a
Cooper Pearson <mptprogesterone@gmail.com>
2020-08-02 14:26:15 -0500

    server fixed

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ import { ui } from './ui';
 // Get posts on DOM load
 document.addEventListener('DOMContentLoaded', getPosts);
 
+//Listen for add post
+
+//Create getPosts function
 function getPosts() {
   http.get('http://localhost:3000/posts')
     .then(data => ui.showPosts(data))
